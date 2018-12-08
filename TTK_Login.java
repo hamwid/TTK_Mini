3f03928b7ca20f1db7ddc9d4c97eaefc5e37c871
hamwid <hampus.widell@alleman.nu>
2018-12-08 17:10:54 +0100

    Added creation of user-list, and attemptLogin()

--- TTK_Login.java
+++ TTK_Login.java
@@ -1,3 +1,15 @@
 public class TTK_Login {
-
+  public static TTK_Users users;
+  public TTK_Login(TTK_Users existingUsers){
+    users = existingUsers;
+  }
+  public static boolean attemptLogin(String username, String password){
+    for(int i=0;i<users.getUsers().size();i++){
+      if((users.getUsers().get(i).getUsername() == username) &&
+         (users.getUsers().get(i).getPassowrd() == password)){
+           return true;
+         }
+    }
+    return false;
+  }
 }
