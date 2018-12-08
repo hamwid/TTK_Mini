17fd19619c3ef939cfb375d4dfdc74d4e25607a4
hamwid <hampus.widell@alleman.nu>
2018-12-08 16:27:43 +0100

    Added private class User and ArraList<User>

--- TTK_Users.java
+++ TTK_Users.java
@@ -1,3 +1,23 @@
-public class TTK_Users {
+import java.util.ArrayList;
 
+public class TTK_Users {
+  ArrayList<User> users = new ArrayList<User>();
+}
+
+private class User {
+  private String username;
+  private String password;
+
+  public void setUsername(String input){
+    username = input;
+  }
+  public void setPassowrd(String input){
+    password = input;
+  }
+  public string getUsername(String input){
+    return username;
+  }
+  public string getPassowrd(String input){
+    return password;
+  }
 }
