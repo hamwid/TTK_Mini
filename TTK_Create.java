44de65e90975cfcff7a46a50af525365dbbc3c49
hamwid <hampus.widell@alleman.nu>
2018-12-08 17:08:46 +0100

    Added getUserbase()

--- TTK_Create.java
+++ TTK_Create.java
@@ -1,6 +1,9 @@
 public class TTK_Create {
   public static TTK_Users users = new TTK_Users();
-  public static void createUser(String username, String password){
+  public void createUser(String username, String password){
     users.addUser(username, password);
+  }
+  public TTK_Users getUserbase(){
+    return users;
   }
 }
